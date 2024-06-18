57a4d3c1ec617456e99a944cd800bb653d6f804c
aryanraj07 <aryanraj010010@gmail.com>
2024-06-19 01:28:57 +0530

    created component setup

---
+++ src/components/WeatherDisplay.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Search from "./Search";
+
+const WeatherDisplay = () => {
+  return (
+    <>
+      <Search />
+      Weather
+    </>
+  );
+};
+
+export default WeatherDisplay;
