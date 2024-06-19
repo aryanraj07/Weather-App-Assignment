bcf91d8d8b39bd5c3d4606fd171c7030452e5cad
aryanraj07 <aryanraj010010@gmail.com>
2024-06-19 20:01:37 +0530

    improved security by adding dotenv file

--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -1,6 +1,6 @@
 import { DateTime } from "luxon";
 
-const API_KEY = "b149c070c3688ad14867669047f30b61";
+const API_KEY = import.meta.env.REACT_APP_API_KEY;
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 const getWeatherData = async (infoType, searchParams) => {
